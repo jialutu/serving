a0f00afcf0ec8f9d77bf3d083246728c2da6a9b2
Jialu <jialutu@gmail.com>
2017-08-01 18:51:06 +0100

    .

--- tensorflow_serving/keras/vgg_client.py
+++ tensorflow_serving/keras/vgg_client.py
@@ -21,14 +21,24 @@ FLAGS = tf.app.flags.FLAGS
 
 host, port = FLAGS.server.split(':')
 
-request = predict_pb2.PredictRequest()
-request.model_spec.name = 'vgg19'
-request.model_spec.signature_name = 'predict'
-request.inputs['images'].CopyFrom(tf.contrib.util.make_tensor_proto(img))
+def main(_):
+  host, port = FLAGS.server.split(':')
+  channel = implementations.insecure_channel(host, int(port))
+  stub = prediction_service_pb2.beta_create_PredictionService_stub(channel)
+  # Send request
+  with open(FLAGS.image, 'rb') as f:
+    # See prediction_service.proto for gRPC request/response details.
+    data = f.read()
+    request = predict_pb2.PredictRequest()
+    request.model_spec.name = 'vgg19'
+    request.model_spec.signature_name = 'predict_images'
+    request.inputs['images'].CopyFrom(
+        tf.contrib.util.make_tensor_proto(data, shape=[1]))
+    result = stub.Predict(request, 10.0)  # 10 secs timeout
+    # to_decode = np.expand_dims(result.outputs['outputs'].float_val, axis=0)
+    # decoded = decode_predictions(to_decode, 5)
+    print(result)
 
-channel = implementations.insecure_channel(host, int(port))
-stub = prediction_service_pb2.beta_create_PredictionService_stub(channel)
-result = stub.Predict(request, 10.0)  # 10 secs timeout
-to_decode = np.expand_dims(result.outputs['outputs'].float_val, axis=0)
-decoded = decode_predictions(to_decode, 5)
-print(decoded)
+
+if __name__ == '__main__':
+  tf.app.run()
